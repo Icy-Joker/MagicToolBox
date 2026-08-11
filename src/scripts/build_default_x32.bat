Icy-Joker/CustomSDK

添加自定义构建脚本

src/scripts/build_default_x32.bat
@@ -1,0 +1,61 @@
+chcp 65001
+
+@echo off
+
+::
+setlocal EnableExtensions
+:: 开启延迟变量()
+setlocal EnableDelayedExpansion
+
+:: 项目解决方案名称
+set PROJECT_NAME=CustomSDK
+
+:: %SecondParty%
+if not defined SecondParty (
+  set SecondParty=
+)
+:: %ThirdParty%
+if not defined ThirdParty (
+  set ThirdParty=
+)
+:: %BOOST_DIR%
+if not defined BOOST_DIR (
+  set BOOST_DIR=%ThirdParty%/boost
+)
+:: %QT_DIR%
+if not defined QT_DIR (
+  set QT_DIR=%ThirdParty%/Qt
+)
+
+pushd "%~dp0%"
+  :: 此处必须注意.为了保持目录结构，脚本名称必须以"build_"开头
+  set SCRIPT_NAME=%~n0
+  :: 源码目录
+  pushd "../%PROJECT_NAME%"
+    set SOURCE_DIRECTORY=%cd%
+  popd
+  :: CMake生成项目解決方案所在目录
+  set BUILD_DIRECTORY=../build/%SCRIPT_NAME:~6%
+  if NOT EXIST "%BUILD_DIRECTORY%" (
+    mkdir "%BUILD_DIRECTORY%"
+  )
+  pushd "%BUILD_DIRECTORY%"
+    :: 删除已有CMakeCache
+    if EXIST "CMakeCache.txt" (
+      del "CMakeCache.txt"
+    )
+    :: 生成解決方案
+    cmake -A "win32" "%SOURCE_DIRECTORY%"
+    :: 自动化步骤
+    if !errorlevel! EQU 0 (
+      ::支持一键打开顶目解决方案
+      choice /t 5 /d N /m "Do you want to open the solution?"
+      if !errorlevel! EQU 1 (
+        start %PROJECT_NAME%.sln
+      )
+    ) else (
+      :: 若CMake执行失败则暂停命令行以查看错误信息
+      pause
+    )
+  popd
+popd

src/scripts/build_default_x64.bat
@@ -1,0 +1,61 @@
+chcp 65001
+
+@echo off
+
+::
+setlocal EnableExtensions
+:: 开启延迟变量()
+setlocal EnableDelayedExpansion
+
+:: 项目解决方案名称
+set PROJECT_NAME=CustomSDK
+
+:: %SecondParty%
+if not defined SecondParty (
+  set SecondParty=
+)
+:: %ThirdParty%
+if not defined ThirdParty (
+  set ThirdParty=
+)
+:: %BOOST_DIR%
+if not defined BOOST_DIR (
+  set BOOST_DIR=%ThirdParty%/boost
+)
+:: %QT_DIR%
+if not defined QT_DIR (
+  set QT_DIR=%ThirdParty%/Qt
+)
+
+pushd "%~dp0%"
+  :: 此处必须注意.为了保持目录结构，脚本名称必须以"build_"开头
+  set SCRIPT_NAME=%~n0
+  :: 源码目录
+  pushd "../%PROJECT_NAME%"
+    set SOURCE_DIRECTORY=%cd%
+  popd
+  :: CMake生成项目解決方案所在目录
+  set BUILD_DIRECTORY=../build/%SCRIPT_NAME:~6%
+  if NOT EXIST "%BUILD_DIRECTORY%" (
+    mkdir "%BUILD_DIRECTORY%"
+  )
+  pushd "%BUILD_DIRECTORY%"
+    :: 删除已有CMakeCache
+    if EXIST "CMakeCache.txt" (
+      del "CMakeCache.txt"
+    )
+    :: 生成解決方案
+    cmake -A "x64" "%SOURCE_DIRECTORY%"
+    :: 自动化步骤
+    if !errorlevel! EQU 0 (
+      ::支持一键打开顶目解决方案
+      choice /t 5 /d N /m "Do you want to open the solution?"
+      if !errorlevel! EQU 1 (
+        start %PROJECT_NAME%.sln
+      )
+    ) else (
+      :: 若CMake执行失败则暂停命令行以查看错误信息
+      pause
+    )
+  popd
+popd

src/scripts/build_linux.sh
@@ -1,0 +1,39 @@
+#!/bin/bash
+
+PROJECT_NAME=CustomSDK #工程名称
+
+export SecondParty=~/WorkSpace/CustomSDK #$SecondParty
+export ThirdParty=/opt/homebrew/opt #$ThirdParty
+export BOOST_DIR=$ThirdParty/boost #$BOOST_DIR
+export QT_DIR=$ThirdParty/qt #$QT_DIR
+
+SCRIPT_NAME=`basename -s ".sh" $0` #确定脚本名称
+SCRIPT_DIRECTORY=`dirname $0` #记录脚本初始目录
+pushd "$SCRIPT_DIRECTORY"
+  pushd "../$PROJECT_NAME"
+    SOURCE_DIRECTORY=$PWD #记录项目源码目录
+  popd
+  BUILD_DIRECTORY=../$SCRIPT_NAME #项目工程目录
+  if ! test -d "$BUILD_DIRECTORY"
+  then
+    mkdir -p "$BUILD_DIRECTORY"
+  fi
+  pushd "$BUILD_DIRECTORY"
+    #if test -f "CMakeCache.txt" #清理CMakeCache.txt
+    #then
+    # rm "CMakeCache.txt"
+    #fi
+    cmake  -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="Release" "$SOURCE_DIRECTORY" #构建项目工程
+    if [ $? -eq 0 ];
+    then
+      read -t 5 -n 1 -p "Do you want to execute make command? [Y/N]: " option
+      case "$option" in
+        [Yy])
+          echo ""
+          make -j12
+          make install
+          ;;
+      esac
+    fi
+  popd
+popd

src/scripts/build_sublime_text.sh
@@ -1,0 +1,43 @@
+#!/bin/bash
+
+PROJECT_NAME=CustomSDK #工程名称
+
+export SecondParty=~/WorkSpace/CustomSDK #$SecondParty
+export ThirdParty=/opt/homebrew/opt #$ThirdParty
+export BOOST_DIR=$ThirdParty/boost #$BOOST_DIR
+export QT_DIR=$ThirdParty/qt #$QT_DIR
+
+SCRIPT_NAME=`basename -s ".sh" $0` #确定脚本名称
+SCRIPT_DIRECTORY=`dirname $0` #记录脚本初始目录
+pushd "$SCRIPT_DIRECTORY"
+  pushd "../$PROJECT_NAME"
+    SOURCE_DIRECTORY=$PWD #记录项目源码目录
+  popd
+  BUILD_DIRECTORY=../$SCRIPT_NAME #项目工程目录
+  if ! test -d "$BUILD_DIRECTORY"
+  then
+    mkdir -p "$BUILD_DIRECTORY"
+  fi
+  pushd "$BUILD_DIRECTORY"
+    #if test -f "CMakeCache.txt" #清理CMakeCache.txt
+    #then
+    # rm "CMakeCache.txt"
+    #fi
+    cmake  -G "Sublime Text 2 - Unix Makefiles" -DCMAKE_BUILD_TYPE="Release" "$SOURCE_DIRECTORY" #构建项目工程
+    if [ $? -eq 0 ];
+    then
+      read -t 5 -n 1 -p "Do you want to open the project with \"Sublime Text\"? [Y/N]: " option
+      case "$option" in
+        [Yy])
+          echo ""
+          if test "subl"
+          then
+            subl --launch-or-new-window --project "$PROJECT_NAME.sublime-project"
+          else
+            open "$PROJECT_NAME.sublime-project"
+          fi
+          ;;
+      esac
+    fi
+  popd
+popd

src/scripts/build_vc100_x32.bat
@@ -1,0 +1,61 @@
+chcp 65001
+
+@echo off
+
+::
+setlocal EnableExtensions
+:: 开启延迟变量()
+setlocal EnableDelayedExpansion
+
+:: 项目解决方案名称
+set PROJECT_NAME=CustomSDK
+
+:: %SecondParty%
+if not defined SecondParty (
+  set SecondParty=
+)
+:: %ThirdParty%
+if not defined ThirdParty (
+  set ThirdParty=
+)
+:: %BOOST_DIR%
+if not defined BOOST_DIR (
+  set BOOST_DIR=%ThirdParty%/boost
+)
+:: %QT_DIR%
+if not defined QT_DIR (
+  set QT_DIR=%ThirdParty%/Qt
+)
+
+pushd "%~dp0%"
+  :: 此处必须注意.为了保持目录结构，脚本名称必须以"build_"开头
+  set SCRIPT_NAME=%~n0
+  :: 源码目录
+  pushd "../%PROJECT_NAME%"
+    set SOURCE_DIRECTORY=%cd%
+  popd
+  :: CMake生成项目解決方案所在目录
+  set BUILD_DIRECTORY=../build/%SCRIPT_NAME:~6%
+  if NOT EXIST "%BUILD_DIRECTORY%" (
+    mkdir "%BUILD_DIRECTORY%"
+  )
+  pushd "%BUILD_DIRECTORY%"
+    :: 删除已有CMakeCache
+    if EXIST "CMakeCache.txt" (
+      del "CMakeCache.txt"
+    )
+    :: 生成解決方案
+    cmake -G "Visual Studio 10 2010" -A "win32" "%SOURCE_DIRECTORY%"
+    :: 自动化步骤
+    if !errorlevel! EQU 0 (
+      ::支持一键打开顶目解决方案
+      choice /t 5 /d N /m "Do you want to open the solution?"
+      if !errorlevel! EQU 1 (
+        start %PROJECT_NAME%.sln
+      )
+    ) else (
+      :: 若CMake执行失败则暂停命令行以查看错误信息
+      pause
+    )
+  popd
+popd

src/scripts/build_vc100_x64.bat
@@ -1,0 +1,61 @@
+chcp 65001
+
+@echo off
+
+::
+setlocal EnableExtensions
+:: 开启延迟变量()
+setlocal EnableDelayedExpansion
+
+:: 项目解决方案名称
+set PROJECT_NAME=CustomSDK
+
+:: %SecondParty%
+if not defined SecondParty (
+  set SecondParty=
+)
+:: %ThirdParty%
+if not defined ThirdParty (
+  set ThirdParty=
+)
+:: %BOOST_DIR%
+if not defined BOOST_DIR (
+  set BOOST_DIR=%ThirdParty%/boost
+)
+:: %QT_DIR%
+if not defined QT_DIR (
+  set QT_DIR=%ThirdParty%/Qt
+)
+
+pushd "%~dp0%"
+  :: 此处必须注意.为了保持目录结构，脚本名称必须以"build_"开头
+  set SCRIPT_NAME=%~n0
+  :: 源码目录
+  pushd "../%PROJECT_NAME%"
+    set SOURCE_DIRECTORY=%cd%
+  popd
+  :: CMake生成项目解決方案所在目录
+  set BUILD_DIRECTORY=../build/%SCRIPT_NAME:~6%
+  if NOT EXIST "%BUILD_DIRECTORY%" (
+    mkdir "%BUILD_DIRECTORY%"
+  )
+  pushd "%BUILD_DIRECTORY%"
+    :: 删除已有CMakeCache
+    if EXIST "CMakeCache.txt" (
+      del "CMakeCache.txt"
+    )
+    :: 生成解決方案
+    cmake -G "Visual Studio 10 2010" -A "x64" "%SOURCE_DIRECTORY%"
+    :: 自动化步骤
+    if !errorlevel! EQU 0 (
+      ::支持一键打开顶目解决方案
+      choice /t 5 /d N /m "Do you want to open the solution?"
+      if !errorlevel! EQU 1 (
+        start %PROJECT_NAME%.sln
+      )
+    ) else (
+      :: 若CMake执行失败则暂停命令行以查看错误信息
+      pause
+    )
+  popd
+popd

src/scripts/build_vc140_x32.bat
@@ -1,0 +1,61 @@
+chcp 65001
+
+@echo off
+
+::
+setlocal EnableExtensions
+:: 开启延迟变量()
+setlocal EnableDelayedExpansion
+
+:: 项目解决方案名称
+set PROJECT_NAME=CustomSDK
+
+:: %SecondParty%
+if not defined SecondParty (
+  set SecondParty=
+)
+:: %ThirdParty%
+if not defined ThirdParty (
+  set ThirdParty=
+)
+:: %BOOST_DIR%
+if not defined BOOST_DIR (
+  set BOOST_DIR=%ThirdParty%/boost
+)
+:: %QT_DIR%
+if not defined QT_DIR (
+  set QT_DIR=%ThirdParty%/Qt
+)
+
+pushd "%~dp0%"
+  :: 此处必须注意.为了保持目录结构，脚本名称必须以"build_"开头
+  set SCRIPT_NAME=%~n0
+  :: 源码目录
+  pushd "../%PROJECT_NAME%"
+    set SOURCE_DIRECTORY=%cd%
+  popd
+  :: CMake生成项目解決方案所在目录
+  set BUILD_DIRECTORY=../build/%SCRIPT_NAME:~6%
+  if NOT EXIST "%BUILD_DIRECTORY%" (
+    mkdir "%BUILD_DIRECTORY%"
+  )
+  pushd "%BUILD_DIRECTORY%"
+    :: 删除已有CMakeCache
+    if EXIST "CMakeCache.txt" (
+      del "CMakeCache.txt"
+    )
+    :: 生成解決方案
+    cmake -G "Visual Studio 14 2015" -A "win32" "%SOURCE_DIRECTORY%"
+    :: 自动化步骤
+    if !errorlevel! EQU 0 (
+      ::支持一键打开顶目解决方案
+      choice /t 5 /d N /m "Do you want to open the solution?"
+      if !errorlevel! EQU 1 (
+        start %PROJECT_NAME%.sln
+      )
+    ) else (
+      :: 若CMake执行失败则暂停命令行以查看错误信息
+      pause
+    )
+  popd
+popd

src/scripts/build_vc143_x32.bat
@@ -1,0 +1,61 @@
+chcp 65001
+
+@echo off
+
+::
+setlocal EnableExtensions
+:: 开启延迟变量()
+setlocal EnableDelayedExpansion
+
+:: 项目解决方案名称
+set PROJECT_NAME=CustomSDK
+
+:: %SecondParty%
+if not defined SecondParty (
+  set SecondParty=
+)
+:: %ThirdParty%
+if not defined ThirdParty (
+  set ThirdParty=
+)
+:: %BOOST_DIR%
+if not defined BOOST_DIR (
+  set BOOST_DIR=%ThirdParty%/boost
+)
+:: %QT_DIR%
+if not defined QT_DIR (
+  set QT_DIR=%ThirdParty%/Qt
+)
+
+pushd "%~dp0%"
+  :: 此处必须注意.为了保持目录结构，脚本名称必须以"build_"开头
+  set SCRIPT_NAME=%~n0
+  :: 源码目录
+  pushd "../%PROJECT_NAME%"
+    set SOURCE_DIRECTORY=%cd%
+  popd
+  :: CMake生成项目解決方案所在目录
+  set BUILD_DIRECTORY=../build/%SCRIPT_NAME:~6%
+  if NOT EXIST "%BUILD_DIRECTORY%" (
+    mkdir "%BUILD_DIRECTORY%"
+  )
+  pushd "%BUILD_DIRECTORY%"
+    :: 删除已有CMakeCache
+    if EXIST "CMakeCache.txt" (
+      del "CMakeCache.txt"
+    )
+    :: 生成解決方案
+    cmake -G "Visual Studio 17 2022" -A "win32" "%SOURCE_DIRECTORY%"
+    :: 自动化步骤
+    if !errorlevel! EQU 0 (
+      ::支持一键打开顶目解决方案
+      choice /t 5 /d N /m "Do you want to open the solution?"
+      if !errorlevel! EQU 1 (
+        start %PROJECT_NAME%.sln
+      )
+    ) else (
+      :: 若CMake执行失败则暂停命令行以查看错误信息
+      pause
+    )
+  popd
+popd

Icy-Joker/MagicToolBox

src/scripts/build_default_x32.bat
@@ -1,0 +1,61 @@
+chcp 65001
+
+@echo off
+
+::
+setlocal EnableExtensions
+:: 开启延迟变量()
+setlocal EnableDelayedExpansion
+
+:: 项目解决方案名称
+set PROJECT_NAME=MagicToolBox
+
+:: %SecondParty%
+if not defined SecondParty (
+  set SecondParty=
+)
+:: %ThirdParty%
+if not defined ThirdParty (
+  set ThirdParty=
+)
+:: %BOOST_DIR%
+if not defined BOOST_DIR (
+  set BOOST_DIR=%ThirdParty%/boost
+)
+:: %QT_DIR%
+if not defined QT_DIR (
+  set QT_DIR=%ThirdParty%/qt
+)
+
+pushd "%~dp0%"
+  :: 此处必须注意.为了保持目录结构，脚本名称必须以"build_"开头
+  set SCRIPT_NAME=%~n0
+  :: 源码目录
+  pushd "../%PROJECT_NAME%"
+    set SOURCE_DIRECTORY=%cd%
+  popd
+  :: CMake生成项目解決方案所在目录
+  set BUILD_DIRECTORY=../build/%SCRIPT_NAME:~6%
+  if NOT EXIST "%BUILD_DIRECTORY%" (
+    mkdir "%BUILD_DIRECTORY%"
+  )
+  pushd "%BUILD_DIRECTORY%"
+    :: 删除已有CMakeCache
+    if EXIST "CMakeCache.txt" (
+      del "CMakeCache.txt"
+    )
+    :: 生成解決方案
+    cmake -A "win32" "%SOURCE_DIRECTORY%"
+    :: 自动化步骤
+    if !errorlevel! EQU 0 (
+      ::支持一键打开顶目解决方案
+      choice /t 5 /d N /m "Do you want to open the solution?"
+      if !errorlevel! EQU 1 (
+        start %PROJECT_NAME%.sln
+      )
+    ) else (
+      :: 若CMake执行失败则暂停命令行以查看错误信息
+      pause
+    )
+  popd
+popd

src/scripts/build_default_x64.bat
@@ -1,0 +1,61 @@
+chcp 65001
+
+@echo off
+
+::
+setlocal EnableExtensions
+:: 开启延迟变量()
+setlocal EnableDelayedExpansion
+
+:: 项目解决方案名称
+set PROJECT_NAME=MagicToolBox
+
+:: %SecondParty%
+if not defined SecondParty (
+  set SecondParty=
+)
+:: %ThirdParty%
+if not defined ThirdParty (
+  set ThirdParty=
+)
+:: %BOOST_DIR%
+if not defined BOOST_DIR (
+  set BOOST_DIR=%ThirdParty%/boost
+)
+:: %QT_DIR%
+if not defined QT_DIR (
+  set QT_DIR=%ThirdParty%/qt
+)
+
+pushd "%~dp0%"
+  :: 此处必须注意.为了保持目录结构，脚本名称必须以"build_"开头
+  set SCRIPT_NAME=%~n0
+  :: 源码目录
+  pushd "../%PROJECT_NAME%"
+    set SOURCE_DIRECTORY=%cd%
+  popd
+  :: CMake生成项目解決方案所在目录
+  set BUILD_DIRECTORY=../build/%SCRIPT_NAME:~6%
+  if NOT EXIST "%BUILD_DIRECTORY%" (
+    mkdir "%BUILD_DIRECTORY%"
+  )
+  pushd "%BUILD_DIRECTORY%"
+    :: 删除已有CMakeCache
+    if EXIST "CMakeCache.txt" (
+      del "CMakeCache.txt"
+    )
+    :: 生成解決方案
+    cmake -A "x64" "%SOURCE_DIRECTORY%"
+    :: 自动化步骤
+    if !errorlevel! EQU 0 (
+      ::支持一键打开顶目解决方案
+      choice /t 5 /d N /m "Do you want to open the solution?"
+      if !errorlevel! EQU 1 (
+        start %PROJECT_NAME%.sln
+      )
+    ) else (
+      :: 若CMake执行失败则暂停命令行以查看错误信息
+      pause
+    )
+  popd
+popd

src/scripts/build_linux.sh
@@ -1,0 +1,39 @@
+#!/bin/bash
+
+PROJECT_NAME=MagicToolBox #工程名称
+
+export SecondParty=~/WorkSpace/MagicToolBox #$SecondParty
+export ThirdParty=/opt/homebrew/opt #$ThirdParty
+export BOOST_DIR=$ThirdParty/boost #$BOOST_DIR
+export QT_DIR=$ThirdParty/qt #$QT_DIR
+
+SCRIPT_NAME=`basename -s ".sh" $0` #确定脚本名称
+SCRIPT_DIRECTORY=`dirname $0` #记录脚本初始目录
+pushd "$SCRIPT_DIRECTORY"
+  pushd "../$PROJECT_NAME"
+    SOURCE_DIRECTORY=$PWD #记录项目源码目录
+  popd
+  BUILD_DIRECTORY=../$SCRIPT_NAME #项目工程目录
+  if ! test -d "$BUILD_DIRECTORY"
+  then
+    mkdir -p "$BUILD_DIRECTORY"
+  fi
+  pushd "$BUILD_DIRECTORY"
+    #if test -f "CMakeCache.txt" #清理CMakeCache.txt
+    #then
+    # rm "CMakeCache.txt"
+    #fi
+    cmake  -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="Release" "$SOURCE_DIRECTORY" #构建项目工程
+    if [ $? -eq 0 ];
+    then
+      read -t 5 -n 1 -p "Do you want to execute make command? [Y/N]: " option
+      case "$option" in
+        [Yy])
+          echo ""
+          make -j12
+          make install
+          ;;
+      esac
+    fi
+  popd
+popd

src/scripts/build_sublime_text.sh
@@ -1,0 +1,43 @@
+#!/bin/bash
+
+PROJECT_NAME=MagicToolBox #工程名称
+
+export SecondParty=~/WorkSpace/MagicToolBox #$SecondParty
+export ThirdParty=/opt/homebrew/opt #$ThirdParty
+export BOOST_DIR=$ThirdParty/boost #$BOOST_DIR
+export QT_DIR=$ThirdParty/qt #$QT_DIR
+
+SCRIPT_NAME=`basename -s ".sh" $0` #确定脚本名称
+SCRIPT_DIRECTORY=`dirname $0` #记录脚本初始目录
+pushd "$SCRIPT_DIRECTORY"
+  pushd "../$PROJECT_NAME"
+    SOURCE_DIRECTORY=$PWD #记录项目源码目录
+  popd
+  BUILD_DIRECTORY=../$SCRIPT_NAME #项目工程目录
+  if ! test -d "$BUILD_DIRECTORY"
+  then
+    mkdir -p "$BUILD_DIRECTORY"
+  fi
+  pushd "$BUILD_DIRECTORY"
+    #if test -f "CMakeCache.txt" #清理CMakeCache.txt
+    #then
+    # rm "CMakeCache.txt"
+    #fi
+    cmake  -G "Sublime Text 2 - Unix Makefiles" -DCMAKE_BUILD_TYPE="Release" "$SOURCE_DIRECTORY" #构建项目工程
+    if [ $? -eq 0 ];
+    then
+      read -t 5 -n 1 -p "Do you want to open the project with \"Sublime Text\"? [Y/N]: " option
+      case "$option" in
+        [Yy])
+          echo ""
+          if test "subl"
+          then
+            subl --launch-or-new-window --project "$PROJECT_NAME.sublime-project"
+          else
+            open "$PROJECT_NAME.sublime-project"
+          fi
+          ;;
+      esac
+    fi
+  popd
+popd

src/scripts/build_vc100_x32.bat
@@ -1,0 +1,62 @@
+chcp 65001
+
+@echo off
+
+::
+setlocal EnableExtensions
+:: 开启延迟变量()
+setlocal EnableDelayedExpansion
+
+:: 项目解决方案名称
+set PROJECT_NAME=MagicToolBox
+
+:: %SecondParty%
+if not defined SecondParty (
+  set SecondParty=
+)
+:: %ThirdParty%
+if not defined ThirdParty (
+  set ThirdParty=
+)
+:: %BOOST_DIR%
+if not defined BOOST_DIR (
+  set BOOST_DIR=%ThirdParty%/boost
+)
+:: %QT_DIR%
+if not defined QT_DIR (
+  set QT_DIR=%ThirdParty%/qt
+)
+
+pushd "%~dp0%"
+  :: 此处必须注意.为了保持目录结构，脚本名称必须以"build_"开头
+  set SCRIPT_NAME=%~n0
+  :: 源码目录
+  pushd "../%PROJECT_NAME%"
+    set SOURCE_DIRECTORY=%cd%
+  popd
+  :: CMake生成项目解決方案所在目录
+  set BUILD_DIRECTORY=../build/%SCRIPT_NAME:~6%
+  if NOT EXIST "%BUILD_DIRECTORY%" (
+    mkdir "%BUILD_DIRECTORY%"
+  )
+  pushd "%BUILD_DIRECTORY%"
+    :: 删除已有CMakeCache
+    if EXIST "CMakeCache.txt" (
+      del "CMakeCache.txt"
+    )
+    :: 生成解決方案
+    :: cmake -G "Visual Studio 10 2010" -A "win32" -T "v100" "%SOURCE_DIRECTORY%"
+    cmake -A "win32" -T "v100" "%SOURCE_DIRECTORY%"
+    :: 自动化步骤
+    if !errorlevel! EQU 0 (
+      ::支持一键打开顶目解决方案
+      choice /t 5 /d N /m "Do you want to open the solution?"
+      if !errorlevel! EQU 1 (
+        start %PROJECT_NAME%.sln
+      )
+    ) else (
+      :: 若CMake执行失败则暂停命令行以查看错误信息
+      pause
+    )
+  popd
+popd

src/scripts/build_vc100_x64.bat
@@ -1,0 +1,62 @@
+chcp 65001
+
+@echo off
+
+::
+setlocal EnableExtensions
+:: 开启延迟变量()
+setlocal EnableDelayedExpansion
+
+:: 项目解决方案名称
+set PROJECT_NAME=MagicToolBox
+
+:: %SecondParty%
+if not defined SecondParty (
+  set SecondParty=
+)
+:: %ThirdParty%
+if not defined ThirdParty (
+  set ThirdParty=
+)
+:: %BOOST_DIR%
+if not defined BOOST_DIR (
+  set BOOST_DIR=%ThirdParty%/boost
+)
+:: %QT_DIR%
+if not defined QT_DIR (
+  set QT_DIR=%ThirdParty%/qt
+)
+
+pushd "%~dp0%"
+  :: 此处必须注意.为了保持目录结构，脚本名称必须以"build_"开头
+  set SCRIPT_NAME=%~n0
+  :: 源码目录
+  pushd "../%PROJECT_NAME%"
+    set SOURCE_DIRECTORY=%cd%
+  popd
+  :: CMake生成项目解決方案所在目录
+  set BUILD_DIRECTORY=../build/%SCRIPT_NAME:~6%
+  if NOT EXIST "%BUILD_DIRECTORY%" (
+    mkdir "%BUILD_DIRECTORY%"
+  )
+  pushd "%BUILD_DIRECTORY%"
+    :: 删除已有CMakeCache
+    if EXIST "CMakeCache.txt" (
+      del "CMakeCache.txt"
+    )
+    :: 生成解決方案
+    :: cmake -G "Visual Studio 10 2010" -A "x64" -T "v100" "%SOURCE_DIRECTORY%"
+    cmake -A "x64" -T "v100" "%SOURCE_DIRECTORY%"
+    :: 自动化步骤
+    if !errorlevel! EQU 0 (
+      ::支持一键打开顶目解决方案
+      choice /t 5 /d N /m "Do you want to open the solution?"
+      if !errorlevel! EQU 1 (
+        start %PROJECT_NAME%.sln
+      )
+    ) else (
+      :: 若CMake执行失败则暂停命令行以查看错误信息
+      pause
+    )
+  popd
+popd

src/scripts/build_vc140_x32.bat
@@ -1,0 +1,62 @@
+chcp 65001
+
+@echo off
+
+::
+setlocal EnableExtensions
+:: 开启延迟变量()
+setlocal EnableDelayedExpansion
+
+:: 项目解决方案名称
+set PROJECT_NAME=MagicToolBox
+
+:: %SecondParty%
+if not defined SecondParty (
+  set SecondParty=
+)
+:: %ThirdParty%
+if not defined ThirdParty (
+  set ThirdParty=
+)
+:: %BOOST_DIR%
+if not defined BOOST_DIR (
+  set BOOST_DIR=%ThirdParty%/boost
+)
+:: %QT_DIR%
+if not defined QT_DIR (
+  set QT_DIR=%ThirdParty%/qt
+)
+
+pushd "%~dp0%"
+  :: 此处必须注意.为了保持目录结构，脚本名称必须以"build_"开头
+  set SCRIPT_NAME=%~n0
+  :: 源码目录
+  pushd "../%PROJECT_NAME%"
+    set SOURCE_DIRECTORY=%cd%
+  popd
+  :: CMake生成项目解決方案所在目录
+  set BUILD_DIRECTORY=../build/%SCRIPT_NAME:~6%
+  if NOT EXIST "%BUILD_DIRECTORY%" (
+    mkdir "%BUILD_DIRECTORY%"
+  )
+  pushd "%BUILD_DIRECTORY%"
+    :: 删除已有CMakeCache
+    if EXIST "CMakeCache.txt" (
+      del "CMakeCache.txt"
+    )
+    :: 生成解決方案
+    :: cmake -G "Visual Studio 14 2015" -A "win32" -T "v140" "%SOURCE_DIRECTORY%"
+    cmake -A "win32" -T "v140" "%SOURCE_DIRECTORY%"
+    :: 自动化步骤
+    if !errorlevel! EQU 0 (
+      ::支持一键打开顶目解决方案
+      choice /t 5 /d N /m "Do you want to open the solution?"
+      if !errorlevel! EQU 1 (
+        start %PROJECT_NAME%.sln
+      )
+    ) else (
+      :: 若CMake执行失败则暂停命令行以查看错误信息
+      pause
+    )
+  popd
+popd

src/scripts/build_vc143_x32.bat
@@ -1,0 +1,62 @@
+chcp 65001
+
+@echo off
+
+::
+setlocal EnableExtensions
+:: 开启延迟变量()
+setlocal EnableDelayedExpansion
+
+:: 项目解决方案名称
+set PROJECT_NAME=MagicToolBox
+
+:: %SecondParty%
+if not defined SecondParty (
+  set SecondParty=
+)
+:: %ThirdParty%
+if not defined ThirdParty (
+  set ThirdParty=
+)
+:: %BOOST_DIR%
+if not defined BOOST_DIR (
+  set BOOST_DIR=%ThirdParty%/boost
+)
+:: %QT_DIR%
+if not defined QT_DIR (
+  set QT_DIR=%ThirdParty%/qt
+)
+
+pushd "%~dp0%"
+  :: 此处必须注意.为了保持目录结构，脚本名称必须以"build_"开头
+  set SCRIPT_NAME=%~n0
+  :: 源码目录
+  pushd "../%PROJECT_NAME%"
+    set SOURCE_DIRECTORY=%cd%
+  popd
+  :: CMake生成项目解決方案所在目录
+  set BUILD_DIRECTORY=../build/%SCRIPT_NAME:~6%
+  if NOT EXIST "%BUILD_DIRECTORY%" (
+    mkdir "%BUILD_DIRECTORY%"
+  )
+  pushd "%BUILD_DIRECTORY%"
+    :: 删除已有CMakeCache
+    if EXIST "CMakeCache.txt" (
+      del "CMakeCache.txt"
+    )
+    :: 生成解決方案
+    :: cmake -G "Visual Studio 17 2022" -A "win32" -T "v143" "%SOURCE_DIRECTORY%"
+    cmake -A "win32" -T "v143" "%SOURCE_DIRECTORY%"
+    :: 自动化步骤
+    if !errorlevel! EQU 0 (
+      ::支持一键打开顶目解决方案
+      choice /t 5 /d N /m "Do you want to open the solution?"
+      if !errorlevel! EQU 1 (
+        start %PROJECT_NAME%.sln
+      )
+    ) else (
+      :: 若CMake执行失败则暂停命令行以查看错误信息
+      pause
+    )
+  popd
+popd